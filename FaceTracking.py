0fda44464f53b1d7155492ad358900b97d6e349e
hqjang <hqjang95@gmail.com>
2020-10-07 15:45:17 +0900

    step2 face detection

--- FaceTracking.py
+++ FaceTracking.py
@@ -7,6 +7,8 @@ myDrone = initTello()
 while True:
     #1단계. cv2 라이브러리를 활용해 텔로의 카메라화면 보여주기
     img = getTelloFrame(myDrone,w,h)
+    #2단계. 얼굴정보를 가져오기
+    img,faceInfo = findFace(img) #faceInfo[0][0]은 얼굴 중심 x좌표, faceInfo[0][1]은 얼굴 중심 y좌표, faceInfo[1]은 얼굴면적
     cv2.imshow('Image',img)
     #멈추기. 안전상으로 필요한 코드
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- TelloUtils.py
+++ TelloUtils.py
@@ -1,6 +1,7 @@
 from djitellopy.tello import Tello
 import cv2
 
+#step1
 def initTello():
     myDrone = Tello()
     myDrone.connect()
@@ -19,4 +20,27 @@ def getTelloFrame(myDrone, w=360, h=240):
     IMG = cv2.resize(myFrame,(w,h))
     return IMG
 
+#step2
+def findFace(img):
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BAYER_BG2GRAY)
+    faces = faceCascade.detectMultiScale(imgGray,1.2,4) #1.2,4는 scalefactor, 다른값으로 바꿔도 된다.
 
+    #프레임마다 두 리스트에 얼굴 면적, 중심점 정보를 계속 집어넣는다.
+    FaceCenterList = []
+    FaceAreaList = []
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cx = x + w // 2
+        cy = y + h // 2
+        area = w * h
+        FaceAreaList.append(area)
+        FaceCenterList.append([cx, cy])
+
+    #리스트의 맨 마지막 부분의 idx를 가져와 면적과 중심정보를 리턴한다.
+    if len(FaceAreaList):
+        idx = FaceAreaList.index(max(FaceAreaList))
+        return img, [FaceCenterList[idx], FaceAreaList[idx]]
+    else:
+        return img, [[0, 0], 0]
